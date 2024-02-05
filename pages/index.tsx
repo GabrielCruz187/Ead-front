994bcaf0f720dac76d596d44bd95e2055da5238a
Gabriel Cruz <gabicraque10@outlook.com>
2024-02-05 13:59:07 -0300

    Configuração incial do projeto, configurando arquivos e pastas. Instalação do sass e do bootstrap.

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import styles from "../styles/homeNoAuth.module.scss" 
+
+
+const HomeNotAuth = function () {
+  return (
+		<>
+			<Head></Head>
+			<main></main>
+		</>
+};
+
+export default HomeNotAuth;
