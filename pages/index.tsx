2020cc4a66409779daf07b8aa8157b532f7c627c
Gabriel Cruz <gabicraque10@outlook.com>
2024-02-05 14:24:30 -0300

    pastas/components

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,15 @@ import styles from "../styles/homeNoAuth.module.scss"
 const HomeNotAuth = function () {
   return (
 		<>
-			<Head></Head>
+			<Head>
+      <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+				<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."></meta>
+      </Head>
 			<main></main>
 		</>
+  );
 };
 
 export default HomeNotAuth;
